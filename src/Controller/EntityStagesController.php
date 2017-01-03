09f0e18a295d28be66d15cd106ce364a55b0c842
Luis SEVERINO <lfs.severino@gmail.com>
2017-01-03 01:54:11 +0100

    Update link conditions

--- src/Controller/EntityStagesController.php
+++ src/Controller/EntityStagesController.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\entity_stages\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Url;
 use \Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -20,6 +23,23 @@ class EntityStagesController extends ControllerBase {
     ];
 
     return $build;
+  }
+
+  /**
+   * Publish action.
+   */
+  public function publishAction($entity_id) {
+    // Update node info.
+    $nodeLoad = Node::load($entity_id);
+    $nodeLoad->set('status', 1)->save();
+    // Redirect.
+    $targetUrl = Url::fromRoute(
+    'view.entity_stages.default_page',
+    [],
+    ['absolute' => TRUE]
+    )->toString();
+
+    return new RedirectResponse($targetUrl);
   }
 
 }
