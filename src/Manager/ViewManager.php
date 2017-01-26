79141f6521b34114de1ccff730e09474f3f0176b
Luis SEVERINO <lfs.severino@gmail.com>
2017-01-26 22:21:44 +0100

    Update query revison results

--- src/Manager/ViewManager.php
+++ src/Manager/ViewManager.php
@@ -28,6 +28,11 @@ class ViewManager {
       foreach ($variables['rows'] as $key => &$row) {
         // Get comparable revisions values.
         $revisionEntity = $view->result[$key]->_entity;
+        if(!method_exists($revisionEntity, 'id')) {
+          // kpr($view->result[$key]);
+          // kpr($revisionEntity);
+          // die();
+        }
         $currentEntity = Node::load($revisionEntity->id());
 
         // Create Operation URLs.
@@ -172,12 +177,12 @@ class ViewManager {
 
       // Update rows number and pager.
       // $view->pager->setItemsPerPage(1000);.
-      $nbRows = count($view->result);
-      $view->total_items = $nbRows;
-      $view->pager->total_items = $nbRows;
-      $view->pager->updatePageInfo();
-      $view->query->view->pager->total_items = $nbRows;
-      $view->query->view->pager->updatePageInfo();
+      // $nbRows = count($view->result);
+      // $view->total_items = $nbRows;
+      // $view->pager->total_items = $nbRows;
+      // $view->pager->updatePageInfo();
+      // $view->query->view->pager->total_items = $nbRows;
+      // $view->query->view->pager->updatePageInfo();
     }
   }
 
@@ -185,21 +190,26 @@ class ViewManager {
    * Implements hook_views_query_alter().
    */
   public function _viewsQueryAlter(ViewExecutable $view, QueryPluginBase $query) {
+    // select * from node_field_revision nfr right join node_field_data nfd ON nfr.nid=nfd.nid left join node_revision nr ON nfr.vid = nr.vid  where nfr.vid != nfd.vid AND nr.revision_uid NOT IN (1) ORDER BY nr.revision_timestamp DESC \G
+
     // Mysql 5.7.16 -> SET SQL_MODE="";.
     if ($view->storage->get('id') == 'entity_stages') {
+      // Services.
       $settings = Settings::getAll();
       $currentUser = \Drupal::currentUser();
       $getRequest = \Drupal::request();
+      $getViewsJoinManager = \Drupal::service('plugin.manager.views.join');
+
+      // Request.
       $getType = $getRequest->get('type');
       $getNid = $getRequest->get('nid');
       $getOrder = $getRequest->get('order');
-      $getViewsJoinManager = \Drupal::service('plugin.manager.views.join');
-      $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
 
       // Update query definitions.
-      $query->fields['langcode']['table'] = 'node_revision';
-      $query->addTable('node_revision');
-      $query->addTable('node_field_revision');
+      $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+      $query->fields['langcode']['table'] = 'node_field_revision';
+      $query->addField('node_field_revision', 'nid', 'node_field_revision_nid', ['functions' => 'groupby']);
+
 
       // Update sort by type table.
       $query->orderby[0]['field'] = $getOrder == 'type' ? 'nfd.type' : $query->orderby[0]['field'];
@@ -208,101 +218,131 @@ class ViewManager {
         'node_field_revision.vid',
         $query->orderby[0]['field']
       );
-      // Distinct values by user uid and entity nid.
-      $query->addGroupBy('nfd.nid', 'DESC');
-      $query->addGroupBy('node_revision.nid');
-      $query->addGroupBy('node_field_revision.nid');
-      // kpr($query->groupby);die();
-      // Get User Admin and User where content is auto validated.
-      $entityQuery = \Drupal::entityQuery('user');
-      $entityQuery->condition('roles', 'administrator');
-      $entity_ids = $entityQuery->execute();
+
+      // Filter by type.
+      foreach($query->where[1]['conditions'] as &$condition) {
+        if($condition['field'] == 'node_field_revision.type'){
+          if($getType == 'All' || empty($getType)){
+            $condition = [
+              'field' => 'nfd.type',
+              'value' => "NOT NULL",
+              'operator' => '=',
+            ];
+          }
+          else {
+            $condition = [
+              'field' => 'nfd.type',
+              'value' => $query->where[1]['conditions'][0]['value'],
+              'operator' => '=',
+            ];
+          }
+        }
+      }
 
       // Join's.
       $join = $getViewsJoinManager->createInstance(
-        'standard',
-        [
-          'type' => 'INNER',
-          'table' => 'node_field_data',
-          'field' => 'nid',
-          'left_table' => 'node_revision',
-          'left_field' => 'nid',
-          'operator' => '=',
-          'adjusted' => TRUE,
-          'extraOperator' => 'AND',
-          'extra' => array(
-            0 => array(
-              'left_field' => 'vid',
-              'field' => 'vid',
-              'operator' => '!=',
-            ),
-            1 => array(
-              'field' => 'langcode',
-              'value' => $language,
-              'operator' => '=',
-            ),
+      'standard',
+      [
+        'type' => 'LEFT',
+        'table' => 'node_field_data',
+        'field' => 'nid',
+        'left_table' => 'node_field_revision',
+        'left_field' => 'nid',
+        'operator' => '=',
+        'adjusted' => TRUE,
+        'extraOperator' => 'AND',
+        'extra' => array(
+          0 => array(
+            'left_field' => 'vid',
+            'field' => 'vid',
+            'operator' => '!=',
           ),
-        ]
-      );
-
-      $joinUserRoles = $getViewsJoinManager->createInstance(
-        'standard',
-        [
-          'type' => 'INNER',
-          'table' => 'user__roles',
-          'field' => 'entity_id',
-          'left_table' => 'node_revision',
-          'left_field' => 'revision_uid',
-          'operator' => '=',
-          'adjusted' => TRUE,
-          'extraOperator' => 'AND',
-          'extra' => array(
-            0 => array(
-              'field' => 'entity_id',
-              'value' => $entity_ids,
-              'operator' => 'NOT IN',
-            ),
+          2 => array(
+            'left_field' => 'uid',
+            'field' => 'uid',
+            'operator' => '=',
           ),
+          1 => array(
+            'field' => 'langcode',
+            'value' => $language,
+            'operator' => '=',
+          ),
+        ),
         ]
       );
 
       // Add left join for node table from the node_revision table.
       $query->addRelationship('nfd', $join, 'node_field_data');
-      $query->addRelationship('ur', $joinUserRoles, 'user__roles');
 
-      // Filter by type.
-      if ($getType == 'All' || empty($getType)) {
-        $query->where[1]['conditions'] = [];
-      }
-      else {
-        $query->where[1]['conditions'][0] = [
-          'field' => 'nfd.type',
-          'value' => $query->where[1]['conditions'][0]['value'],
-          'operator' => '=',
-        ];
-      }
+      // Add condition based on allowed revisions.
       $query->where[1]['conditions'][] = [
-        'field' => 'node_field_revision.entity_stages_revision_status',
-        'value' => NULL,
-        'operator' => 'IS NULL',
+        'field' => 'node_field_revision.vid',
+        'value' => $this->getPendingRevisions(),
+        'operator' => 'IN',
       ];
-
-      // Filter by node id.
-      if ($getNid && (int) $getNid) {
-        $query->where[1]['conditions'][] = [
-          'field' => 'nfd.nid',
-          'value' => $getNid,
-          'operator' => '=',
-        ];
-      }
     }
+  }
+
+  /**
+   * Return allowed Revisions pending validation.
+   */
+  public function getPendingRevisions($args = []) {
+    // Ignore validated revisions.
+    // Ignore revisions created by anonymous users.
+    $entityQuery = \Drupal::entityQuery('user');
+    $entityQuery->condition('roles', ['administrator']);
+    $entity_ids = $entityQuery->execute() + [0];
+    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+
+    // Filter Node Field Revision Data.
+    $query = \Drupal::database()->select('node_field_revision', 'nfr');
+    // Joins.
+    $query->leftJoin('node_revision', 'nr', 'nfr.vid = nr.vid');
+    $query->leftJoin('node_field_data', 'nfd', 'nfr.nid = nfd.nid AND nfr.vid != nfd.vid ');
+    // Fields.
+    // -- Node field Data.
+    $query->addField('nfd', 'title', 'nfd_title');
+    $query->addField('nfd', 'type', 'nfd_title');
+    // -- Node Revision.
+    $query->addField('nr', 'vid', 'nr_vid');
+    $query->addField('nr', 'revision_uid', 'nr_revision_uid');
+    $query->addField('nr', 'revision_log', 'nfr_revision_log');
+    // -- Node Field Revision.
+    $query->addField('nfr', 'title', 'nfr_title');
+    $query->addField('nfr', 'nid', 'nfr_nid');
+    $query->addField('nfr', 'vid', 'nfr_vid');
+    // Conditions.
+    $query->condition('nr.revision_uid', $entity_ids, 'NOT IN');
+    $query->condition('nr.langcode', $langcode);
+    $query->condition('nfr.langcode', $langcode);
+    $query->condition(
+      db_or()
+        ->condition('nfr.entity_stages_revision_status', NULL, 'IS NULL')
+        ->condition('nfr.entity_stages_revision_status', '4')
+    );
+    $query->condition('nfd.langcode', $langcode);
+    // Order by.
+    $query->orderBy('nr.revision_timestamp', 'DESC');
+    // Execute and fetch results.
+    $pendingValidationRevisions = $query->execute()->fetchAll();
+    // Extract valid vid.
+    $trueVid = [];
+    // Do not change this! Reverse order to override last value to a given vid.
+    krsort($pendingValidationRevisions);
+    foreach ($pendingValidationRevisions as $key => $value) {
+      $trueVid[$value->nfr_nid] = $value->nfr_vid;
+    }
+    // Array values;
+    $trueVid = array_values($trueVid);
+
+    return $trueVid;
   }
 
   /**
    * Implements hook_views_data_alter().
    */
   public function _viewsDataAlter(array &$data) {
-    $data['node_revision']['type'] = $data['node_field_data']['type'];
+    $data['node_field_revision']['type'] = $data['node_field_data']['type'];
   }
 
 }
